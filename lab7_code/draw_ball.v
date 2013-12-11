84682698aae141c011844ccfa4880de20a5e8768
Luke <efajk.cs01@nctu.edu.tw>
2013-12-11 14:52:55 +0800

    modified drawball

--- lab7_code/draw_ball.v
+++ lab7_code/draw_ball.v
@@ -25,6 +25,7 @@ module draw_ball(
 // I/O ports declearation
 input [10:0] vcounter;
 input [11:0] hcounter;
+input  [1:0] color;
 input        visible;
 input  [2:0] radius;
 input [10:0] ball_x;
@@ -35,7 +36,30 @@ output       VGA_B;
 
 //--------------------------------------------------------------------------------
 // Internal signal
+wire [31:0] ball_size;
+// (X-X0)^2 +(Y-Y0)^2 <= r^2
+assign ball_size = ((hcounter - ball_x)*(hcounter - ball_x)+(vcounter - ball_y)*(vcounter - ball_y));
 
+alwyas(*)
+begin 
+  if(visible)begin
+    if(ball_size <= ((radius*5)*(radius*5)))
+      begin
+        if(color == 2'd1)
+          VGA_R = 1'b1; VGA_G = 1'b0; VGA_B = 1'b0;
+        else if(color == 2'd2)
+          VGA_R = 1'b0; VGA_G = 1'b1; VGA_B = 1'b0;
+        else if(color == 2'd3)
+          VGA_R = 1'b0; VGA_G = 1'b0; VGA_B = 1'b1;
+      end
+    else begin
+       VGA_R = 1'b0; VGA_G = 1'b0; VGA_B = 1'b0;
+    end
+  else begin
+      VGA_R = 1'b0; VGA_G = 1'b0; VGA_B = 1'b0;
+  end
+end
+           
 //--------------------------------------------------------------------------------
 // Parameter declearation
 
